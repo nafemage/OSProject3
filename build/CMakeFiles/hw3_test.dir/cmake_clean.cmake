372697b8cddfbfc4750819b13d6232d35e02a6aa
jlpt1 <89395854+jlpt1@users.noreply.github.com>
2024-03-01 13:43:02 -0600

    test

---
+++ build/CMakeFiles/hw3_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw3_test.dir/test/tests.cpp.obj"
+  "CMakeFiles/hw3_test.dir/test/tests.cpp.obj.d"
+  "hw3_test.exe"
+  "hw3_test.exe.manifest"
+  "hw3_test.pdb"
+  "libhw3_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw3_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
